e4f890d189d93f79d61a4dd9a27be95ffd8a1bcf
andre <andre@bambu-server1>
2023-12-31 19:50:33 -0300

    Dockerfile for ndnSIM v2.9 in Ubuntu 20.04

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,15 +1,17 @@
 #!/bin/bash
 
+mkdir -p /ndnSIM/pybindgen
+mkdir -p /ndnSIM/ns-3
 cd /ndnSIM
 if [ -z "$( ls ./pybindgen )" ]; then  
     git config --global http.version HTTP/1.1 && \
-    git clone -b 0.21.0 https://github.com/named-data-ndnSIM/pybindgen.git pybindgen 
+    git clone -b 0.22.1 https://github.com/named-data-ndnSIM/pybindgen.git pybindgen 
 fi
 cd /ndnSIM
 if [ -z "$( ls ./ns-3 )" ]; then    
     git config --global http.version HTTP/1.1 && \
-    git clone -b ndnSIM-ns-3.30.1             https://github.com/named-data-ndnSIM/ns-3-dev.git  ns-3 && 
-    git clone -b ndnSIM-2.8       --recursive https://github.com/named-data-ndnSIM/ndnSIM.git    ns-3/src/ndnSIM &&     
+    git clone -b ndnSIM-ns-3.35                   https://github.com/named-data-ndnSIM/ns-3-dev.git  ns-3 && 
+    git clone -b ndnSIM-2.9-NFD-22.02 --recursive https://github.com/named-data-ndnSIM/ndnSIM.git    ns-3/src/ndnSIM &&     
     cd ./ns-3 &&
     ./waf configure -d debug --enable-examples
 fi
